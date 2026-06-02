7fbc22a478101143f27dc04b2fcb6bd7457b5e74
Zaryab Raza <zaryab.dev01@gmail.com>
2026-06-02 14:25:49 +0500

    debug(build): add diagnostics for deployment data fetching issues

--- src/app/(public)/our-team/page.tsx
+++ src/app/(public)/our-team/page.tsx
@@ -2,6 +2,8 @@ import { getTeamMembers } from "@/lib/content";
 import OurTeamClient from "./OurTeamClient";
 
 export default async function OurTeamPage() {
+  console.log("Fetching team...");
   const teamMembers = await getTeamMembers();
+  console.log("Team fetched:", teamMembers.length);
   return <OurTeamClient teamMembers={teamMembers} />;
 }
--- src/app/(public)/our-work/page.tsx
+++ src/app/(public)/our-work/page.tsx
@@ -2,6 +2,8 @@ import { getProjects } from "@/lib/content";
 import OurWorkClient from "./OurWorkClient";
 
 export default async function OurWorkPage() {
+  console.log("Fetching projects...");
   const projects = await getProjects();
+  console.log("Projects fetched:", projects.length);
   return <OurWorkClient projects={projects} />;
 }
